74720decb57af7370bf841ae086d93a7a7abe24f
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-05-01 15:20:15 +0200

    add scripts

---
+++ addons/analytic/migrations/10.0.1.1/post-migration.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+def deactivate_closed_accounts(env):
+    accounts = env['account.analytic.account'].search([])
+    for account in accounts:
+        if account.account_type == 'closed':
+            account.active == False
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    deactivate_closed_accounts(env)
---
+++ addons/analytic/migrations/10.0.1.1/pre-migration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    openupgrade.copy_columns( cr, {
+        'account.analytic.account' : [('account_type', None, None)]
+    })
+
+
